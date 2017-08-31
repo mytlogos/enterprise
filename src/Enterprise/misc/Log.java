18fd95b2103b2bc82017f47a61d52fd645b3e20a
Mytlogos <mytlogos@hotmail.com>
2017-08-31 09:41:32 +0200

    centralized logger

--- src/Enterprise/misc/Log.java
+++ src/Enterprise/misc/Log.java
@@ -9,44 +9,77 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 /**
+ * Utility class to provide a {@link Logger} with
+ * several default methods.
+ * Default setting is that it saves the logs in the file, while appending,
+ * not creating a new file every time.
+ * Creates the directory "log" in the working directory, if it does not exist.
  *
  */
 public class Log {
 
+    /**
+     * Counter for {@link #checkDirectory(String)}.
+     */
     private static int counter = 0;
 
+    /**
+     * // TODO: 31.08.2017 do the doc
+     *
+     * @param logClass
+     * @return
+     */
     public static Logger classLogger(Class<?> logClass) {
-        Logger logger = Logger.getLogger(logClass.getSimpleName());
-
-        addFileHandler(logger, logClass.getSimpleName());
-
-        return logger;
+        return getLogger(logClass.getSimpleName());
     }
 
+    /**
+     * // TODO: 31.08.2017 do the doc
+     *
+     * @param logInstance
+     * @return
+     */
     public static Logger classLogger(Object logInstance) {
-        Logger logger = Logger.getLogger(logInstance.getClass().getSimpleName());
-
-        addFileHandler(logger, logInstance.getClass().getSimpleName());
-
-        return logger;
+        return getLogger(logInstance.getClass().getSimpleName());
     }
 
+    /**
+     * // TODO: 31.08.2017 do the doc
+     *
+     * @param logClass
+     * @return
+     */
     public static Logger packageLogger(Class<?> logClass) {
-        Logger logger = Logger.getLogger(logClass.getPackage().getName());
-
-        addFileHandler(logger, logClass.getPackage().getName());
-
-        return logger;
+        return getLogger(logClass.getPackage().getName());
     }
 
+    /**
+     * // TODO: 31.08.2017 do the doc
+     *
+     * @param logInstance
+     * @return
+     */
     public static Logger packageLogger(Object logInstance) {
-        Logger logger = Logger.getLogger(logInstance.getClass().getPackage().getName());
+        return getLogger(logInstance.getClass().getPackage().getName());
+    }
 
-        addFileHandler(logger, logInstance.getClass().getPackage().getName());
-
+    /**
+     * // TODO: 31.08.2017 do the doc
+     *
+     * @param name
+     * @return
+     */
+    private static Logger getLogger(String name) {
+        Logger logger = Logger.getLogger(name);
+        addFileHandler(logger, name);
         return logger;
     }
 
+    /**
+     * // TODO: 31.08.2017 do the doc
+     * @param logger
+     * @param fileName
+     */
     private static void addFileHandler(Logger logger, String fileName) {
         String directory = "log\\";
         try {
@@ -61,9 +94,20 @@ public class Log {
             }
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("logs will not be saved in a file");
         }
     }
 
+    /**
+     *
+     * // TODO: 31.08.2017 do the doc
+     * Counts the number of times it tried to check the directory.
+     *  Aborts if it tried more than 10 times.
+     *
+     * @param directory
+     * @return
+     * @throws IOException
+     */
     private static boolean checkDirectory(String directory) throws IOException {
         boolean exists = false;
         if (counter > 10) {
@@ -79,7 +123,7 @@ public class Log {
         } else if (Files.exists(directoryPath)) {
             exists = true;
         } else {
-            System.out.println("programm hat nicht die nötigen rechte um den pfad abzufragen");
+            System.out.println("program does not the necessary rights to query the directory");
         }
         return exists;
     }
